3fcbab250fa453b64f9b1cc2035457899984a1f4
Karina Kozarova <karinakozarova1303@gmail.com>
2017-03-24 21:17:13 +0200

    Add files via upload

---
+++ start.ino
@@ -0,0 +1,29 @@
+int xPin = A1;
+int yPin = A0;
+
+int xPosition = 0;
+int yPosition = 0;
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+}
+
+
+void loop() {
+
+}
+
+void joystick_position(){
+  int xValue=analogRead(xPin);
+  Serial.println(xPosition);
+  delay(1);
+  int yValue=analogRead(yPin);
+  Serial.println(yPosition);
+  delay(1);
+  }
+
+
+
+
