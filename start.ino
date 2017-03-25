9d116e45ecc19968f5e2775a9a8b62d7367c62f3
Karina Kozarova <karinakozarova1303@gmail.com>
2017-03-25 10:23:01 +0200

    Update start.ino

--- start.ino
+++ start.ino
@@ -1,6 +1,5 @@
 int xPin = A1;
 int yPin = A0;
-int ledPin = 8;
 int xPosition = 0;
 int yPosition = 0;
 
@@ -9,85 +8,66 @@ void move_forward();
 void move_left();
 void move_right();
 void move_down();
+void print_joystick_position(int xValue,int yValue);
 
 void setup() {
   Serial.begin(9600); 
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
-  /*pinMode(ledPin,OUTPUT);*/
 }
 
 void loop() {
   joystick_position();
 
 }
-
-void joystick_position(){
-  int xValue=analogRead(xPin);
+void print_joystick_position(int xValue,int yValue){
   Serial.print("X: ");
   Serial.print(xValue);
   delay(250);
-  int yValue=analogRead(yPin);
   Serial.print("Y: ");
   Serial.println(yValue);
   delay(250);
-  /*digitalWrite(ledPin,xValue*50);*/
-  if(xValue==507){
-      if(yValue==504 || yValue==505 || yValue==503){
-            Serial.print("Button not moved");
-          }
+  }
+void joystick_position(){
+  int xValue=analogRead(xPin);
+  int yValue=analogRead(yPin);
+ 
+  if(xValue==507&&(yValue==504 || yValue==505 || yValue==503)){
+            Serial.println("Button not moved");
     }
     
-   if(xValue > 1020){
-    if(yValue >501 && yValue<504){
+   if(xValue > 1020&&yValue >501 && yValue<504){
       move_forward();
     }
-    }
     
-  if(xValue<6){
-    if(yValue> 479&&yValue<509)
-    {
+  if(xValue<6&&yValue> 479&&yValue<509){
       move_down();
-    }
    } 
     
-  if(yValue<5){
-    if(xValue >505 && xValue <595){
-    move_left();
-    }
+  if(yValue<5&&xValue >505 && xValue <595){
+      move_left();
   } 
- if(xValue > 473&& xValue<508) {
-  if(yValue ==1023){
-  move_right();
- }
+ if(xValue > 473&& xValue<508&&yValue ==1023) {
+      move_right();
  }
 }
 
 void move_forward(){
-//x ot 1023 do 1022 
-// y ot 360 do 506
- Serial.print("Forward ");
-joystick_position();
+  Serial.println("Forward ");
+  joystick_position();
 }
 void move_left()
 {
-//x 506 - 594
-// y 0
- Serial.print("Left ");
-
-joystick_position();
+ Serial.println("Left ");
+ joystick_position();
 }
 void move_right()
 {
-//x 474-507
-// y 1023
- Serial.print("Right: ");
-joystick_position();
+  Serial.println("Right: ");
+  joystick_position();
 }
 void move_down()
 {
-// x 0
-//y 480 -504
- Serial.print("Down ");
-joystick_position();
+  Serial.println("Down ");
+  joystick_position();
 }
