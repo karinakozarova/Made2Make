2d27f5de29cfb4b69a4714dac4c9d8307542121e
Karina Kozarova <karinakozarova1303@gmail.com>
2017-03-25 13:19:51 +0200

    Added Servo moving up and down
    Made our servo move up and down depending on the button state

--- start.ino
+++ start.ino
@@ -1,19 +1,32 @@
+#include <Servo.h> 
+ 
+Servo servo_test; 
+                
+int angle = 0;    
 int xPin = A1;
 int yPin = A0;
+int buttonPin = 12;
+
 int xPosition = 0;
 int yPosition = 0;
+int buttonState = 0;
+int servo_up = 0;
 
 void joystick_position();
-void move_forward();
-void move_left();
-void move_right();
-void move_down();
+void joystick_move_forward();
+void joystick_move_left();
+void joystick_move_right();
+void joystick_move_down();
 void print_joystick_position(int xValue,int yValue);
+void servo_move_up();
+void servo_move_down();
+void joystick_onclick();
 
 void setup() {
   Serial.begin(9600); 
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
@@ -33,41 +46,70 @@ void joystick_position(){
   int yValue=analogRead(yPin);
  
   if(xValue==507&&(yValue==504 || yValue==505 || yValue==503)){
-            Serial.println("Button not moved");
+            buttonState = digitalRead(buttonPin);
+            if (buttonState == HIGH) {
+              joystick_onclick();
+              }
+            else Serial.println("Button not moved");
+            
     }
     
    if(xValue > 1020&&yValue >501 && yValue<504){
-      move_forward();
+      joystick_move_forward();
     }
     
   if(xValue<6&&yValue> 479&&yValue<509){
-      move_down();
+      joystick_move_down();
    } 
     
   if(yValue<5&&xValue >505 && xValue <595){
-      move_left();
+      joystick_move_left();
   } 
  if(xValue > 473&& xValue<508&&yValue ==1023) {
-      move_right();
+      joystick_move_right();
  }
 }
 
-void move_forward(){
+void joystick_move_forward(){
   Serial.println("Forward ");
   joystick_position();
 }
-void move_left()
+void joystick_move_left()
 {
  Serial.println("Left ");
  joystick_position();
 }
-void move_right()
+void joystick_move_right()
 {
   Serial.println("Right: ");
   joystick_position();
 }
-void move_down()
+void joystick_move_down()
 {
   Serial.println("Down ");
   joystick_position();
 }
+
+void servo_move_up(){
+  //servo goes up
+  for(angle = 0; angle < 90; angle += 1)    
+  {                                  
+    servo_test.write(angle);                 
+    delay(15);                       
+  } 
+  delay(1000);
+}
+void servo_move_down(){
+  //servo goes down
+  for(angle = 90; angle>=1; angle-=5)    
+  {                                
+    servo_test.write(angle);              
+    delay(5);                       
+  } 
+  delay(1000);
+  servo_up = 0;
+  }
+ void joystick_onclick(){
+  if(servo_up==1){servo_move_down();}
+  else servo_move_up();
+  }
