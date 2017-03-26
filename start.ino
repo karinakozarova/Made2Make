8bc4750c85aa25a74cfda2c59dd728ceadda4efd
Karina Kozarova <karinakozarova1303@gmail.com>
2017-03-26 12:47:07 +0300

    Demo-ready version
    added some comments

--- start.ino
+++ start.ino
@@ -5,42 +5,39 @@
 #define STEP_DURATION 1 //in ms
 #define MOTOR_PLATE 2
 #define MOTOR_PEN 1
+
+#include <Servo.h> 
  
-#include <Servo.h>
- 
-Servo servo_test;
+Servo servo_test; 
 Servo myservo;
-               
-int angle = 0;  
-int down = 0;
+                
+int angle = 0;   
+int down = 0; 
 int xPin = A1;
 int yPin = A0;
 int bPin = A5;
- 
+
 int xDir = 0;
 int yDir = 0;
 int bValue = 0;
 int sValue = 0;
- 
+
 int xPosition = 0;
 int yPosition = 0;
-int buttonState = 0;
-//int servo_up = 0;
 int pos = 0;
- 
+
 int xValue = 0;
 int yValue = 0;
- 
-int state = 0;
- 
+
+
 void joystick_position();
- 
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); 
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
   pinMode(bPin, INPUT);
- 
+  
   pinMode(DIR_MOTOR_PEN,OUTPUT);
   pinMode(STEP_MOTOR_PEN,OUTPUT);
   pinMode(DIR_MOTOR_PLATE,OUTPUT);
@@ -48,57 +45,64 @@ void setup() {
    myservo.attach(6);
    Serial.println("Hello\n");
 }
- 
+
 void loop() {
   joystick_position();
-  //delay(10);
-  //Serial.println("/n");
-  //Serial.println(xValue);
-  //Serial.println(yValue);
- 
 }
- 
+  
 void joystick_position(){
+  //control the joystick
   xValue= analogRead(xPin);
   yValue = analogRead(yPin);
   bValue = analogRead(bPin);
   Serial.print(bValue);
- 
-  sValue = map(bValue,0,1023,85,10);
+
+   //it moves up and down 
+   //chenge pot arduino and it moves the angle of the pen 
+   sValue = map(bValue,0,1023,85,10);
+  
   myservo.write(sValue);
- 
- 
+ // check if x and y position.
   if((xValue>490 && xValue <525 ) && (yValue>490 && yValue<525)){
     stepStop();
                
     } else if((xValue>490 && xValue <525 )){
- 
+
     if(yValue > 525){      
+      
+        //push the y value and it moves the position of the plate right
         yValue = map(yValue,525,1023,20,1);
         yDir = 1;
+       
       } else {
+        //push the y value and it moves the position of the plate left 
         yValue = map(yValue,490,0,20,1);
         yDir = 0;
+      
       }
-     
-      digitalWrite(STEP_MOTOR_PLATE, LOW);
+      
+      digitalWrite(STEP_MOTOR_PLATE, LOW); //
       stepMeHigh(STEP_MOTOR_PEN,yDir,yValue);
- 
+
     } else if((yValue>490 && yValue <525 )){
-     
+      
       if(xValue > 525){      
+        
+        //push the y value and it moves the position of the plate forward 
         xValue = map(xValue,525,1023,20,1);
         xDir = 1;
       } else {
+        
+      
         xValue = map(xValue,490,0,20,1);
         xDir = 0;
       }
- 
+
       digitalWrite(STEP_MOTOR_PEN, LOW);
       stepMeHigh(STEP_MOTOR_PLATE,xDir,xValue);
-     
+      
     } else {
- 
+      //push the y value and it moves the position of the plate backward
       if(xValue > 525){      
         xValue = map(xValue,525,1023,20,1);
         xDir = 1;
@@ -106,7 +110,7 @@ void joystick_position(){
         xValue = map(xValue,490,0,20,1);
         xDir = 0;
       }
-   
+    
       if(yValue > 525){      
         yValue = map(yValue,525,1023,20,1);
         yDir = 1;
@@ -114,30 +118,30 @@ void joystick_position(){
         yValue = map(yValue,490,0,20,1);
         yDir = 0;
       }
-     
+      
       stepMeHigh(STEP_MOTOR_PLATE,xDir,xValue);
-      stepMeHigh(STEP_MOTOR_PEN,yDir,yValue);  
+      stepMeHigh(STEP_MOTOR_PEN,yDir,yValue);   
     }
-   
- 
+    
+  
 }
- 
+
 void servo_move_down(){
-  for (pos = 90; pos >= 0; pos -= 1) {
+  for (pos = 90; pos >= 0; pos -= 1) { 
     myservo.write(pos);                    
   }
 }
- 
+
 void servo_move_up(){
   //servo goes up
+  //change the angle of the pen
   for(angle = 0; angle < 90; angle += 1)    
   {                                  
     servo_test.write(angle);                    
-  }
-  //delay(1000);
+  } 
 }
- 
-void stepMeHigh(int motor, int dir, int del) {
+
+void stepMeHigh(int motor, int dir, int del) { 
   //1 - motor pen, 2 - motor plate // 0 - left, 1 - right //
   digitalWrite(DIR_MOTOR_PLATE, dir);
   digitalWrite(DIR_MOTOR_PEN, dir);
@@ -145,8 +149,12 @@ void stepMeHigh(int motor, int dir, int del) {
   delay(del);
   digitalWrite(motor, LOW);
  }
- 
+
 void stepStop(){
+  
   digitalWrite(STEP_MOTOR_PEN, LOW);
   digitalWrite(STEP_MOTOR_PLATE, LOW);
 }
+
+ 
+ 
